4b31ab474d33997585c29b5dbf2f2894987a5843
Emerson Molett <emerson.molett@gmail.com>
2021-05-15 18:56:16 -0500

    worked on tile light map

--- 17-Mapping-Web/Instructions/static/js/Leaflet-Step-1/logic.js
+++ 17-Mapping-Web/Instructions/static/js/Leaflet-Step-1/logic.js
@@ -17,3 +17,10 @@ var streeMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
     id: "mapbox/streets-v11", 
     accessToken: API_KEY
 }).addTo(myMap);
+
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18, 
+    id: "light-v10",
+    accessToken: API_KEY
+});
