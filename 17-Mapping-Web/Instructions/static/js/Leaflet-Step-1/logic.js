cc1af5dec9453e62ec5c8835b0ca5db79ae82fa6
Emerson Molett <emerson.molett@gmail.com>
2021-05-15 21:54:17 -0500

    updated syntax errors, map appearas with with size & depth of earthquake

--- 17-Mapping-Web/Instructions/static/js/Leaflet-Step-1/logic.js
+++ 17-Mapping-Web/Instructions/static/js/Leaflet-Step-1/logic.js
@@ -1,17 +1,19 @@
+// Create a leaflet map object using "L.map()"
 var myMap = L.map("map", {
-    center: [37, -100],
+    center: [37.09, -110.71],
     zoom: 4
-    // [lightmap, streeMap]
+    // layers: [lightmap, streetMap]
 });
+
 
 var baseMaps = {
     "Light Map": lightmap
 };
 
 // add tile layer
-var streeMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 500,
+    tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
@@ -24,6 +26,8 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
     id: "light-v10",
     accessToken: API_KEY
 });
+
+
 
 // add light-v10 to tilelayers 
 lightmap.addTo(myMap);
@@ -39,48 +43,50 @@ function markerSize(magnitude) {
 // making function for coloring based on number, generate marker color based on size of earthquake
 var markerColor = function (value) {
     if (value < 0) {
-        return "#c7e76d"
+        return '#c7e76d'
     } else if (value < 20) {
-        return "#b3cf62"
+        return '#b3cf62'
     } else if (value < 60) {
-        return "#9fb857"
+        return '#9fb857'
     } else if (value < 120) {
-        return "#8ba14c"
+        return '#8ba14c'
     } else if (value < 180) {
-        return "#778a41"
+        return '#778a41'
     } else {
-        return "#637336"
+        return '#637336'
     }
 }
 
 // populate markers on map
-d3.json(url).then(function(data) {
+d3.json(url).then(function (data) {
     // go through locations & create city/state marks
-    for (var i = 0; i < data.features.lenght; i++) {
-
+    for (var i = 0; i < data.features.length; i++) {
+        console.log(data.features.length);
+        // Declare variables
         var lat = data.features[i].geometry.coordinates[1];
         var lon = data.features[i].geometry.coordinates[0];
-        var latlon = [lat,lon];
+        var latlon = [lat, lon];
         var mag = data.features[i].properties.mag;
         var depth = data.features[i].geometry.coordinates[2];
         var place = data.features[i].properties.place;
-        // console.log(eaquakeData[i].geometry.coordinates);
-        // console.log(lat,lon);
-        // console.log(place);
+        // console.log(earthquakeData[i].geometry.coordinates)
+        // console.log(lat,lon)
+        // console.log(place)
 
-        // marker radius 
+        // marker radius
         L.circle(latlon, {
-            stroke: "black", 
-            fillOpacity: 0.7, 
-            color: "#cacbcb", 
-            fillColor: markerColor(depth), 
-            fillOpacity: .5,
-            weight: 2,
+            stroke: "black",
+            fillOpacity: 0.5,
+            color: '#FDFEFE',
+            fillColor: markerColor(depth),
+            fillOpacity: .6,
+            weight: 1,
+            //marker size to reflect size// set marker size to reflect the magnitude
+            radius: mag * 25000
+        }).bindPopup(`<h3>Location:${place}</h3><p>Magnitude: ${mag}</p>`)
+            .addTo(myMap);
 
-            //marker size to reflect size
-            radius: mag *25000
-        }).bindPopup("<h3>Location:${place}</h3><p>Magnitude: ${mag}</p>")
-        .addTo(myMap);
+
     }
 })
 
@@ -89,15 +95,15 @@ var legend = L.control({
     position: "bottomright"
 });
 
-legend.onAdd = function() {
+legend.onAdd = function () {
     var div = L.DomUtil.create("div", "legend");
     var limits = [
-        "<0",
-        "0-19",
-        "20-59",
-        "60-119",
-        "120-179",
-        ">180"
+        '<0',
+        '0 - 19',
+        '20 - 59',
+        '60 - 119',
+        '120 - 179',
+        '>180'
     ];
     var colors = [
         "#c7e76d",
@@ -106,19 +112,21 @@ legend.onAdd = function() {
         "8ba14c",
         "#778a41",
         "#637336"
+
     ];
 
     // heading for legend
-    var legendInfo = "<h3> Earthquake Depth</h3>"
+    var legendInfo = "<h3>Earthquake Depth</h3>"
 
-    div.innerHtml = legendInfo;
+    div.innerHTML = legendInfo;
 
-    for (var i = 0; i < limits.length;i++) {
-        div.innerHtml +=
-        '<i style="background:' + (colors[i]) + '"></i> ' +
-                limits[i] + '<br>';
+    for (var i = 0; i < limits.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + (colors[i]) + '"></i> ' +
+            limits[i] + '<br>';
     }
-    return div; 
+
+    return div;
 };
 
 legend.addTo(myMap)
